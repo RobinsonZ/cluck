84f479ad05087d626be0b6f7421cf694ced9c776
Zachary Robinson <robinsonz@catlin.edu>
2018-08-09 21:31:29 -0400

    Remove unused data class
    Signed-off-by: Zachary Robinson <robinsonz@catlin.edu>

--- src/main/kotlin/org/team1540/timeclock/backend/data/DataClasses.kt
+++ src/main/kotlin/org/team1540/timeclock/backend/data/DataClasses.kt
@@ -1,5 +1,3 @@
 package org.team1540.timeclock.backend.data
 
 data class SimpleEmail(val to: String, val subject: String, val text: String)
-
-data class Session(val start: String, val end: String)
