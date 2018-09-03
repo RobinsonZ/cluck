ab2c69ea49925cb77dfc49301d873682098250e8
Zachary Robinson <robinsonz@catlin.edu>
2018-09-03 13:58:32 -0700

    Log authentication tests

--- src/main/kotlin/org/team1540/cluck/backend/controllers/UtilController.kt
+++ src/main/kotlin/org/team1540/cluck/backend/controllers/UtilController.kt
@@ -1,5 +1,6 @@
 package org.team1540.cluck.backend.controllers
 
+import mu.KotlinLogging
 import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -7,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class UtilController {
+
+    val logger = KotlinLogging.logger {}
+
     @RequestMapping("/ping")
     fun ping() {
 
@@ -14,7 +18,15 @@ class UtilController {
 
     @GetMapping("authtest")
     fun testAuth(auth: Authentication?): Map<String, String?> {
-        return mapOf("accessLevel" to (auth?.authorities?.mapNotNull { it.authority }?.map { it.removePrefix("ROLE_") }?.firstOrNull()
-                ?: "NONE"))
+        val authLevel = (auth?.authorities?.mapNotNull { it.authority }?.map { it.removePrefix("ROLE_") }?.firstOrNull()
+                ?: "NONE")
+        logger.debug {
+            if (auth == null) {
+                "Recieved authentication test request from unauthenticated user (access level $authLevel)"
+            } else {
+                "Recieved authentication test request from user ${auth.name} (access level $authLevel)"
+            }
+        }
+        return mapOf("accessLevel" to authLevel)
     }
 }
