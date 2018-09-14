36e86faaad71ba8cfde5881d50a4fa3ba99bb6dc
Zachary Robinson <robinsonz@catlin.edu>
2018-09-13 18:26:34 -0700

    Log IP address in testAuth()

--- src/main/kotlin/org/team1540/cluck/backend/controllers/UtilController.kt
+++ src/main/kotlin/org/team1540/cluck/backend/controllers/UtilController.kt
@@ -5,6 +5,7 @@ import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.servlet.ServletRequest
 
 @RestController
 class UtilController {
@@ -17,14 +18,14 @@ class UtilController {
     }
 
     @GetMapping("authtest")
-    fun testAuth(auth: Authentication?): Map<String, String?> {
+    fun testAuth(auth: Authentication?, request: ServletRequest?): Map<String, String?> {
         val authLevel = (auth?.authorities?.mapNotNull { it.authority }?.map { it.removePrefix("ROLE_") }?.firstOrNull()
                 ?: "NONE")
         logger.debug {
             if (auth == null) {
-                "Received authentication test request from unauthenticated user (access level $authLevel)"
+                "Received authentication test request from unauthenticated user (access level $authLevel) at IP addr ${request?.remoteAddr}"
             } else {
-                "Received authentication test request from user ${auth.name} (access level $authLevel)"
+                "Received authentication test request from user ${auth.name} (access level $authLevel) at IP addr ${request?.remoteAddr}"
             }
         }
         return mapOf("accessLevel" to authLevel)
